794ecbbcc185a4810c009da59e68c73dfee03bef
Md Rashidul Azam (Nishan) <arosh019@gmail.com>
2026-09-17 12:03:09 +0600

    fixed json object issue

--- lib/actions/blog.action.ts
+++ lib/actions/blog.action.ts
@@ -1,14 +1,18 @@
 import { connectDB } from "@/database/db";
 import { BlogModel } from "@/database/models/Blog";
+import {cacheLife} from "next/cache";
 
 export async function getSimilarItems(slug: string) {
+    'use cache';
+    cacheLife('hours');
+
     try {
         await connectDB();
 
-        const blog = await BlogModel.findOne({ slug });
-        const similarItems = await BlogModel.find({ _id: { $ne: blog._id }, tags: blog.tags }).limit(5);
+        const blog = await BlogModel.findOne({ slug }).lean();
+        const similarItems = await BlogModel.find({ _id: { $ne: blog._id }, tags: blog.tags }).limit(5).lean();
 
-        return similarItems;
+        return JSON.parse(JSON.stringify(similarItems));
     } catch (error) {
         console.error(error);
         throw new Error(
